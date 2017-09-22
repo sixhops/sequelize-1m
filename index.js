d7cd18a11a807bd3e1b381fa618e77b79c46a5bd
Steven Peters <steve@Jennas-MacBook-Air.local>
2017-09-22 16:22:40 -0700

    adding comments for function explanations

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
 var db = require('./models');
 
-// db.author.findOne().then(function(author) {
-//   author.createPost({
-//     title: 'Why I hate Sequelize',
-//     content: 'Because its just so finnicky'
-//   }).then(function(post) {
-//     console.log(post.get());
-//   });
-// });
+// This finds one author in the table and then creates
+//  a post by that author
+db.author.findOne().then(function(author) {
+  author.createPost({
+    title: 'Why I hate Sequelize',
+    content: 'Because its just so finnicky'
+  }).then(function(post) {
+    console.log(post.get());
+  });
+});
 
-// db.author.findOne().then(function(author) {
-//   // load posts for this author
-//   author.getPosts().then(function(posts) {
-//     //do something with posts here
-//     console.log(posts);
-//   });
-// });
-//
-// db.author.findOne({
-//   where: {
-//     name: 'Pious Augustus'
-//   }
-// }).then(function(author) {
-//   author.createPost({
-//       title: 'How Sequelize saved my life',
-//       content: 'I was drowning in code...'
-//     }).then(function(post) {
-//       console.log(post.get());
-//     });
-// });
+// This finds one author and then gets all
+//  the posts of that author
+db.author.findOne().then(function(author) {
+  // load posts for this author
+  author.getPosts().then(function(posts) {
+    //do something with posts here
+    console.log(posts);
+  });
+});
 
+// This finds one author with the matching name
+//  and then creates a post by that author
+db.author.findOne({
+  where: {
+    name: 'Pious Augustus'
+  }
+}).then(function(author) {
+  author.createPost({
+      title: 'How Sequelize saved my life',
+      content: 'I was drowning in code...'
+    }).then(function(post) {
+      console.log(post.get());
+    });
+});
+
+// This finds all authors and then "includes" the post
+//  model so that all the author's posts are linked to it
+//  as an object (e.g. "author.posts")
 db.author.findAll({
   include: [db.post]
 }).then(function(authors) {
